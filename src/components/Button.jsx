17c2ccf831b5b95064e16214d42b1bdef7a1fe3a
codemon0486 <john.wang960804@gmail.com>
2025-04-16 19:03:33 +0100

    button edit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-const Button = ({ title, className = "" }) => {
+const Button = ({ title, className = "", ...props }) => {
   return (
     <button
+      {...props}
       className={`px-2 py-1 rounded-md border border-blue-800 ${className}`}
     >
       {title}
