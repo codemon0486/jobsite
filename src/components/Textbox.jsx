f3709261ea93869bd2646e8e635b7de28f3e655d
James <jamespk0309@gmail.com>
2025-04-16 16:03:31 +0100

    changeed text component

--- src/components/Textbox.jsx
+++ src/components/Textbox.jsx
@@ -4,16 +4,17 @@ const Textbox = ({
   placeholder,
   value,
   onChange,
-  formData,
+  name,
   type = "text",
   className = "",
   ...props
 }) => (
   <input
     type={type}
+    name={name} // Add the name prop for form handling
     placeholder={placeholder}
     value={value}
-    onChange={(e) => onChange({ ...formData, [e.target.name]: e.target.value })}
+    onChange={onChange} // Pass the onChange function directly
     className={`border border-[#0B5F94] p-2 rounded-md ${className}`}
     {...props}
   />
